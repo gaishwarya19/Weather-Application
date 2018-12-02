cbd8dfae6929ffef3990c0053c0815379837c32a
godavaaa <godavaaa@mail.uc.edu>
2018-12-02 01:08:53 -0500

    added 3 classfile for newsfeed

---
+++ Newsfeed.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenWeatherService_Class
+{
+    public class Newsfeed
+    {
+        public string status { get; set; }
+        public int totalResults { get; set; }
+        public List<Article> articles { get; set; }
+    }
+}
